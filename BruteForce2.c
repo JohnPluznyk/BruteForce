883233811502dd89153f80f7ff519e799a5aeef8
JohnPluznyk <115428211+JohnPluznyk@users.noreply.github.com>
2023-11-09 14:41:49 -0500

    Update BruteForce2.c

--- BruteForce2.c
+++ BruteForce2.c
@@ -25,24 +25,10 @@ int compareArray(char a[], char b[]){
 }
 
 int cracker(char password[], int size){
-    //char arr[size];  //this an illeagal statment b/c size isnt a constant value so if this goes into stack we get issue
     char *arr;
     arr = malloc(size + 1);  //this is reserving bytes of memory in our heap for arr
 
-/*
-    arr[0] = 'c';
-    arr[1] = 'a';
-    arr[2] = 't';
-*/
     arr[size] = '\0';  //set null terminator as the last character in the array
-
-/*    
-    if(compareArray(b, arr) == 0){
-        printf("great success!\n");
-    }
-    else
-        printf("great failure!\n");
-*/
     
     for(int i = 0; i < size; i++){  //intialize an array of a's
         arr[i] = 'a';
